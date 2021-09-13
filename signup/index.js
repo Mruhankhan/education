18469df85a77d56dec51cd6aa6efa2be475841e1
Mruhankhan <s1942405@online.houstonisd.org>
2021-09-13 18:25:01 -0500

    Work

--- signup/index.js
+++ signup/index.js
@@ -1,7 +1,7 @@
 let form = document.getElementById("form")
 let username = document.getElementById("username")
 let password = document.getElementById("password")
-
+let passwordWord = document.getElementById("password-word")
 let div = document.getElementById("div")
 form.addEventListener("submit", e => {
   e.preventDefault()
@@ -10,5 +10,6 @@ form.addEventListener("submit", e => {
   if (username.value.length > 18) error.push("Please make the username shorter")
   if (password.value.length < 8) error.push("Please make password longer")
   if (password.value.length > 18) error.push("Please make the password shorter")
+  if (username.value.length.match(/[A-Z]+/)) passwordWord.innerText = "sass"
   div.append(error)
 })
