c443a3ef285d3e2c0af67f76eb39f82c0166ef3c
Mruhankhan <s1942405@online.houstonisd.org>
2021-09-18 17:38:42 -0500

    Work

--- advancedVersion/index.js
+++ advancedVersion/index.js
@@ -1,5 +1,7 @@
-let low = document.getElementById("low")
-
-low.addEventListener("click", () => {
+let low = document.getElementById("low").addEventListener("click", () => {
   window.location = "/advancedVersionSetup/low.html"
+})
+
+let middle = document.getElementById("middle").addEventListener("click", () => {
+  window.location = "/advancedVersionSetup/middle.html"
 })
